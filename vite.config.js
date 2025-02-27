08499f6cef7b67fae954951e0a677800a2c29102
Michalis Papapetros <michalispap99@gmail.com>
2025-02-27 05:04:53 +0100

    20th attempt

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
   server: {
@@ -15,5 +19,5 @@ export default defineConfig({
     headers: {
       'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' https://d2tpstq7myhvzo.cloudfront.net *.amazoncognito.com; connect-src 'self' https://*.amazonaws.com https://*.amazoncognito.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; frame-src https://eu-central-1ujdeumbz7.auth.eu-central-1.amazoncognito.com; form-action 'self' https://eu-central-1ujdeumbz7.auth.eu-central-1.amazoncognito.com;"
     }
-  },
-});
+  }
+})
